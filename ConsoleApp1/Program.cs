67c9cc61398639e9e2bebb2810efd6fb33241638
Anna Baseley <abaseley@shiftdigital.com>
2021-04-07 19:38:10 -0400

    In-Class example repository

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalHours; //declaring total hours
+
+            //prompt/input
+            Console.WriteLine("Hello C#! How long is class (in hours)?");
+            string input = Console.ReadLine();
+            //double.Parse is a method that takes in a string and turns it into a double
+            //that is stored in our variable called hours
+            double hours = double.Parse(input);
+
+            //Manipulate
+            totalHours = 0;//mathymaths;
+
+            //Response
+
+
+            int number = 0;
+        }
+    }
+}
